786868421983796dec34de71e812b493be99c984
che_shuai <che_shuai@massclouds.com>
2019-03-08 09:47:08 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 ﻿var app = {
   teams: [
-    { name: '傅斯', tel: '18610279719', email: 'zry@davidche.com', qq: '11111', des: '猥琐大叔嘿嘿', url: 'https://davidsche.github.io/',group:'*' },
-    { name: '冰冰', tel: '15330073261', email: 'wbb@davidche.com', qq: '22222', des: '记人之善，忘人之过', url: 'https://davidsche.github.io/'  ,group:'pro1'},
-    { name: '姗姗', tel: '18201655312', email: 'lushanshan@davidche.com', qq: '22222', des: '运气不会太差', url: 'https://davidsche.github.io/'  ,group:'pro1'}
+    { name: 'AA', tel: '130111111', email: 'aa@davidche.com', qq: '11111', des: '猥琐大叔嘿嘿', url: 'https://davidsche.github.io/',group:'*' },
+    { name: 'BB', tel: '130222222', email: 'bb@davidche.com', qq: '22222', des: '记人之善，忘人之过', url: 'https://davidsche.github.io/'  ,group:'pro1'},
+    { name: 'CC', tel: '130333333', email: 'cc@davidche.com', qq: '22222', des: '运气不会太差', url: 'https://davidsche.github.io/'  ,group:'pro2'}
     ]
 };
